12f809591aa8032955250a556416d383f88deda8
gillandshofwan <gillandshofwan456@gmail.com>
2025-12-11 15:51:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("This is a title")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+st.title("PROJEK LPK BROOOK")
+st.title("_Streamlit_ is :purple[cool] :sunglasses:")
