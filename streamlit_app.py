bb8b72cb77bbd599c030b70fe1521af99ca83471
gillandshofwan <gillandshofwan456@gmail.com>
2025-12-21 23:27:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("Welcom to ADA APA DI LAB",text_alignment="left")
+st.title("Welcom to ADA APA DI LAB",text_alignment="center")
 st.title("_Streamlit_ is :green[cool] :sunglasses:")
