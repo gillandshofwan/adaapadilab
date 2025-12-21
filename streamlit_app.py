31ede5846f00108e7bf90ca10e488ad53db584f5
gillandshofwan <gillandshofwan456@gmail.com>
2025-12-21 23:30:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Welcom to ADA APA DI LAB",text_alignment="center")
-st.title("_Ada_yang_bisa_di_bantu? :green")
+st.title("Ada yang bisa di bantu? :green", 'center')
