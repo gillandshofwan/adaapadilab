91c5b8723240a0b5747e0ac85f098c1722f9ef7f
gillandshofwan <gillandshofwan456@gmail.com>
2025-12-22 12:47:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,13 @@ import streamlit as st
 
 st.title("Welcom to ADA APA DI LAB",text_alignment="center")
 st.title(":green[Ada yang bisa di bantu?]",text_alignment="center")
+
+nama=input("masukan nama=")
+tahun=int(input("masukan tahun lahir="))
+kota=input("masukan asal kota=")
+
+#menghitung usia
+usia=2025-tahun
+
+#kalimat
+print ('halo',nama,'usia anda sekarang adalah',usia,'anda tinggal di kota',kota,)
